067850d122f64876b6e3c4f9d3d677e364bc2c75
anvimade <140235328+anvimade@users.noreply.github.com>
2024-01-18 20:07:24 +0530

    Add files via upload

--- PRODIGY_SD_02.c
+++ PRODIGY_SD_02.c
@@ -1,113 +1,38 @@
 #include <stdio.h>
-
-void convertCelsiusToFahrenheit();
-void convertFahrenheitToCelsius();
-void convertCelsiusToKelvin();
-void convertKelvinToCelsius();
-void convertFahrenheitToKelvin();
-void convertKelvinToFahrenheit();
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int choice;
+    // Seed the random number generator
+    srand(time(NULL));
 
-    printf("Choose an option:\n");
-    printf("1. Celsius to Fahrenheit\n");
-    printf("2. Fahrenheit to Celsius\n");
-    printf("3. Celsius to Kelvin\n");
-    printf("4. Kelvin to Celsius\n");
-    printf("5. Fahrenheit to Kelvin\n");
-    printf("6. Kelvin to Fahrenheit\n");
+    // Generate a random number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
 
-    scanf("%d", &choice);
+    int userGuess;
+    int attempts = 0;
 
-    switch (choice) {
-        case 1:
-            convertCelsiusToFahrenheit();
-            break;
-        case 2:
-            convertFahrenheitToCelsius();
-            break;
-        case 3:
-            convertCelsiusToKelvin();
-            break;
-        case 4:
-            convertKelvinToCelsius();
-            break;
-        case 5:
-            convertFahrenheitToKelvin();
-            break;
-        case 6:
-            convertKelvinToFahrenheit();
-            break;
-        default:
-            printf("Invalid option\n");
-    }
+    printf("Welcome to the Guess the Number Game!\n");
+    printf("I have selected a random number between 1 and 100. Try to guess it.\n");
+
+    do {
+        // Prompt the user to input their guess
+        printf("Enter your guess: ");
+        scanf("%d", &userGuess);
+
+        // Increment the attempts count
+        attempts++;
+
+        // Check if the guess is correct, too high, or too low
+        if (userGuess == secretNumber) {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        } else if (userGuess < secretNumber) {
+            printf("Too low. Try again!\n");
+        } else {
+            printf("Too high. Try again!\n");
+        }
+
+    } while (userGuess != secretNumber);
 
     return 0;
-}
-
-void convertCelsiusToFahrenheit() {
-    double celsius, fahrenheit;
-
-    printf("Enter temperature in Celsius: ");
-    scanf("%lf", &celsius);
-
-    fahrenheit = (celsius * 9 / 5) + 32;
-
-    printf("Temperature in Fahrenheit: %lf\n", fahrenheit);
-}
-
-void convertFahrenheitToCelsius() {
-    double fahrenheit, celsius;
-
-    printf("Enter temperature in Fahrenheit: ");
-    scanf("%lf", &fahrenheit);
-
-    celsius = (fahrenheit - 32) * 5 / 9;
-
-    printf("Temperature in Celsius: %lf\n", celsius);
-}
-
-void convertCelsiusToKelvin() {
-    double celsius, kelvin;
-
-    printf("Enter temperature in Celsius: ");
-    scanf("%lf", &celsius);
-
-    kelvin = celsius + 273.15;
-
-    printf("Temperature in Kelvin: %lf\n", kelvin);
-}
-
-void convertKelvinToCelsius() {
-    double kelvin, celsius;
-
-    printf("Enter temperature in Kelvin: ");
-    scanf("%lf", &kelvin);
-
-    celsius = kelvin - 273.15;
-
-    printf("Temperature in Celsius: %lf\n", celsius);
-}
-
-void convertFahrenheitToKelvin() {
-    double fahrenheit, kelvin;
-
-    printf("Enter temperature in Fahrenheit: ");
-    scanf("%lf", &fahrenheit);
-
-    kelvin = (fahrenheit + 459.67) * 5 / 9;
-
-    printf("Temperature in Kelvin: %lf\n", kelvin);
-}
-
-void convertKelvinToFahrenheit() {
-    double kelvin, fahrenheit;
-
-    printf("Enter temperature in Kelvin: ");
-    scanf("%lf", &kelvin);
-
-    fahrenheit = (kelvin * 9 / 5) - 459.67;
-
-    printf("Temperature in Fahrenheit: %lf\n", fahrenheit);
 }
